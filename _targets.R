10cb55a7425e78ce25a2b0523c7f74b8f6395b9d
Ben Best <ben@ecoquants.com>
2026-04-08 13:03:10 +0200

    tar_make()

--- _targets.R
+++ _targets.R
@@ -8,8 +8,8 @@
 # targets::tar_outdated()                  # see which targets would run
 # targets::tar_manifest()                  # inspect all targets as a data frame
 # targets::tar_unblock_process()           # unblock processes, eg locked db connections
+# targets::tar_meta(fields = error)        # inspect error metadata for all targets
 
 library(targets)
 devtools::load_all(here::here("../calcofi4db"))
-
 build_targets_list()
