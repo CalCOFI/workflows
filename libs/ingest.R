5c9daa6b31172cc5c415ed61bed133906602fb85
Ben Best <ben@ecoquants.com>
2026-04-08 13:03:10 +0200

    tar_make()

--- libs/ingest.R
+++ libs/ingest.R
@@ -1,9 +1,13 @@
 # common settings for all ingest workflows
 # source(here::here("libs/ingest.R")) in each QMD setup chunk
 
-# set TRUE to force full rebuild (delete wrangling DB + parquet);
+# set TRUE to force rebuild of wrangling DB and parquet outputs;
 # FALSE for incremental runs (skip already-ingested data)
 overwrite <- TRUE
+
+# set TRUE to also redo intermediate checkpoints (RDS files, etc.);
+# downloads are always skipped if files exist regardless of this flag
+overwrite_all <- FALSE
 
 # shared Google Drive data root
 dir_data <- "~/My Drive/projects/calcofi/data-public"
